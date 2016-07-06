571e200b1509d1fa5e8df7f425da6f4f0cacd01d
Kevin Ma <kbma.kevin@gmail.com>
2016-07-06 17:27:05 -0400

    demo'd math.random

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,7 +1,7 @@
 /**
  * FileName: app.js
  * 
- * @author Tom Tsiliopoulos
+ * @author Kevin Ma
  * @date July 6, 2016
  * 
  * StudentID: 300867968
@@ -12,5 +12,24 @@
 (function () {
     "use strict";
 
+    // references to HTMLElements
+    var rollButton = document.getElementById("rollButton");
+    var diceResult = document.getElementById("diceResult");
+
+    function init() {
+        console.log('Application Started...');
+    }
+
+    function diceRoll() {
+        var die1 = Math.floor((Math.random() * 6) + 1); // we never get to 6, 0 to 5 so we have to +1
+        var die2 = Math.floor((Math.random() * 6) + 1);
+        var total = die1 + die2;
+        diceResult.innerHTML = "<h2>" + total + "</h2>";
+    }
+
+    rollButton.addEventListener("click", diceRoll);
+
+    // call init functin when window finishes loading
+    window.addEventListener("load", init);
 
 })();
